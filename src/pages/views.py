1f2263c73eefae2c03ecb0bd80c97b5362d31b87
Paul (Ki-Tae) Han <77474211+paulhan93@users.noreply.github.com>
2022-02-24 23:13:45 -0800

    add and set up page templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")  # string of HTML code
+def home_view(request, *args, **kwargs):
+    #return HttpResponse("<h1>Hello World</h1>")  # string of HTML code
+    return render(request, "home.html", {})    # render page using a template file
 
-def about_view(*args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>")  # string of HTML code
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")  # string of HTML code
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
 
-def social_view(*args, **kwargs):
-    return HttpResponse("<h1>Social Page</h1>")  # string of HTML code
+def social_view(request, *args, **kwargs):
+    return render(request, "social.html", {})
