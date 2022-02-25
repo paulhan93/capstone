3603bd01cd54821dc5e3136ea1375cd003ea6644
Paul (Ki-Tae) Han <77474211+paulhan93@users.noreply.github.com>
2022-02-24 21:00:37 -0800

    add Milestones model

--- src/milestones/admin.py
+++ src/milestones/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Milestones    # relative import (same directory)
+
+admin.site.register(Milestones)
--- src/milestones/models.py
+++ src/milestones/models.py
@@ -1,3 +1,7 @@
+from re import M
 from django.db import models
 
 # Create your models here.
+class Milestones(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
