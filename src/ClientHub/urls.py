7acbbfa3292f11e4178a59a33d9f5a901034a194
Paul (Ki-Tae) Han <77474211+paulhan93@users.noreply.github.com>
2022-02-24 21:27:24 -0800

    add example pages

--- src/ClientHub/urls.py
+++ src/ClientHub/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view   # import homepage view
+from pages.views import about_view, home_view, contact_view, social_view   # import homepage view
 
 urlpatterns = [
     path('', home_view, name='home'),   # add homepage view
+    path('about/', about_view,),
+    path('contact/', contact_view,),
+    path('social/', social_view,),
     path('admin/', admin.site.urls),
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h2>")  # string of HTML code
+    return HttpResponse("<h1>Home Page</h1>")  # string of HTML code
+
+def about_view(*args, **kwargs):
+    return HttpResponse("<h1>About Page</h1>")
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")  
+
+def social_view(*args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")  
