ba35d2be2379e5efd8186b46561d12fd9d89bf75
Paul (Ki-Tae) Han <77474211+paulhan93@users.noreply.github.com>
2022-02-28 17:32:11 -0800

    add templates and page inheritance

--- src/clienthub/urls.py
+++ src/clienthub/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 from pages.views import about_view, contact_view, home_view, social_view
 
 urlpatterns = [
-    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
     path('about/', about_view),
     path('contact/', contact_view),
     path('social/', social_view),
