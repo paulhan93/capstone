96165a90dae7527d0de738d2d73ef1be452a53a9
Paul (Ki-Tae) Han <77474211+paulhan93@users.noreply.github.com>
2022-04-08 11:16:48 -0700

    delete main for merge

--- src/milestones/models.py
+++
@@ -1,8 +0,0 @@
-from pydoc import describe
-from statistics import mode
-from django.db import models
-
-# Create your models here.
-class Milestones(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField(blank=True, null=True)
