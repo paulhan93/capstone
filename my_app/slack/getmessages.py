77aa8440d54d132bd8c965db9da23ec3cf3440b1
Jonas Persson <ubernihilist@gmail.com>
2022-05-09 21:05:55 -0700

    Added the alpha version of a page for displaying messages downloaded from Slack on the frontend: http://127.0.0.1:8000/slack/displaymessages.html

--- my_app/slack/getmessages.py
+++ my_app/slack/getmessages.py
@@ -5,44 +5,46 @@ import os
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-# Obscure the token
-us7 = '25d9654e572ecfcf7f917baf48b8b99'
-us6 = '515940184112-5'
-us5 = '325296356918-3'
-us4 = '340653755745-3'
-us3 = 'p-3'
-us2 = 'ox'
-us1 = 'x'
+def get_json_messages():
+    # Obscure the token
+    us7 = '25d9654e572ecfcf7f917baf48b8b99'
+    us6 = '515940184112-5'
+    us5 = '325296356918-3'
+    us4 = '340653755745-3'
+    us3 = 'p-3'
+    us2 = 'ox'
+    us1 = 'x'
+    # Obscure the channel id
+    ci2 = '039N12TVA7'
+    ci1 = 'C'
 
-# Obscure the channel id
-ci2 = '039N12TVA7'
-ci1 = 'C'
+    # Reconstruct tokens
+    user_token = us1 + us2 + us3 + us4 + us5 + us6 + us7
+    channel_id = ci1 + ci2
 
-# Reconstruct tokens
-user_token = us1 + us2 + us3 + us4 + us5 + us6 + us7
-channel_id = ci1 + ci2
+    # WebClient instantiates a client that can call API methods
+    client = WebClient(token=user_token)
+    logger = logging.getLogger(__name__)
+    # Store conversation history
+    conversation_history = []
+    # ID of the channel you want to send the message to
 
-# WebClient instantiates a client that can call API methods
-client = WebClient(token=user_token)
-logger = logging.getLogger(__name__)
-# Store conversation history
-conversation_history = []
-# ID of the channel you want to send the message to
+    try:
+        # Call the conversations.history method using the WebClient
+        # conversations.history returns the first 100 messages by default
+        # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
+        result = client.conversations_history(channel=channel_id)
 
-try:
-    # Call the conversations.history method using the WebClient
-    # conversations.history returns the first 100 messages by default
-    # These results are paginated, see: https://api.slack.com/methods/conversations.history$pagination
-    result = client.conversations_history(channel=channel_id)
+        conversation_history = result["messages"]
 
-    conversation_history = result["messages"]
 
-    # Print results
-    logger.info("{} messages found in {}".format(len(conversation_history), id))
+        # Print results
+        logger.info("{} messages found in {}".format(len(conversation_history), id))
 
-except SlackApiError as e:
-    logger.error("Error creating conversation: {}".format(e))
+    except SlackApiError as e:
+        logger.error("Error creating conversation: {}".format(e))
 
-history = client.api_call(api_method='conversations.history',data={'channel':channel_id})
-print(history)
+    history = client.api_call(api_method='conversations.history',data={'channel':channel_id})
+    
+    return history
 
--- my_app/slack/views.py
+++ my_app/slack/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 from django.views.generic import TemplateView
 
 from .sendmessage import send_message
+from .getmessages import get_json_messages
 
 @api_view(["POST"])
 def sendmessage(request):
@@ -32,9 +33,12 @@ def sendmessage(request):
 
   return HttpResponse("You submitted: \"" + str + "\"")
 
+def get_formatted_messages(request):
+  return HttpResponse(get_json_messages())
+
 # Alternative way to load a html page:
 # class TestSendMessageView(TemplateView):
 #    template_name = "slack/sendmessage.html"
-
+# Dummy text: html = "<b>Slack message data will show here. <br> It is a good platform to learn programming. It is an educational website. Prepare for the Recruitment drive of product based companies like Microsoft, Amazon, Adobe etc with a free online placement preparation course. The course focuses on various MCQ's & Coding question likely to be asked in the interviews & make your upcoming placement season efficient and successful. Also, any geeks can help other geeks by writing articles on the GeeksforGeeks, publishing articles follow few steps that are Articles that need little modification/improvement from reviewers are published first. To quickly get your articles reviewed, please refer existing articles, their formatting style, coding style, and try to make you are close to them. In case you are a beginner, you may refer Guidelines to write an Article</b>"
 # Dummy comment to allow a new commit
 
