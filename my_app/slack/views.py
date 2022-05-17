5b4a117720607d32650447cc22e081ff2b3d5135
Jonas Persson <ubernihilist@gmail.com>
2022-05-16 20:16:15 -0700

    Release: 4G Clinical Slack Widget - Spinning Bird Kick

--- my_app/slack/views.py
+++ my_app/slack/views.py
@@ -7,7 +7,9 @@ from rest_framework.decorators import api_view
 from django.views.generic import TemplateView
 
 from .sendmessage import send_message
-from .getmessages import get_json_messages
+from .getmessages import getmessage
+import json
+from datetime import datetime
 
 @api_view(["POST"])
 def sendmessage(request):
@@ -34,7 +36,14 @@ def sendmessage(request):
   return HttpResponse("You submitted: \"" + str + "\"")
 
 def get_formatted_messages(request):
-  return HttpResponse(get_json_messages())
+  try:
+    channel_enum = request.GET["channel"]
+  except:
+    channel_enum = '0'
+  return HttpResponse(getmessage(channel_enum))
+
+
+ # return HttpResponse(html)
 
 # Alternative way to load a html page:
 # class TestSendMessageView(TemplateView):
@@ -42,3 +51,8 @@ def get_formatted_messages(request):
 # Dummy text: html = "<b>Slack message data will show here. <br> It is a good platform to learn programming. It is an educational website. Prepare for the Recruitment drive of product based companies like Microsoft, Amazon, Adobe etc with a free online placement preparation course. The course focuses on various MCQ's & Coding question likely to be asked in the interviews & make your upcoming placement season efficient and successful. Also, any geeks can help other geeks by writing articles on the GeeksforGeeks, publishing articles follow few steps that are Articles that need little modification/improvement from reviewers are published first. To quickly get your articles reviewed, please refer existing articles, their formatting style, coding style, and try to make you are close to them. In case you are a beginner, you may refer Guidelines to write an Article</b>"
 # Dummy comment to allow a new commit
 
+#  return HttpResponse(get_json_messages())   -- working return message for getting messages from slack
+
+# Working take Slack data and convert to string:
+#  str = json.dumps(get_json_messages().data)
+#  return HttpResponse(str)
